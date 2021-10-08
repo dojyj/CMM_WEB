bae3427b589568f9c85fcc5557a498088a2d03db
dojyj <dojyj@naver.com>
2021-10-08 16:58:43 +0900

    CMM Dashboard UI 구현

--- controller/cmmDashBoard.js
+++ controller/cmmDashBoard.js
@@ -1,6 +1,25 @@
+import { HA_API } from "./api.js";
 
-function init() {
-    
+const activeIp = document.getElementById('active-ip');
+const activePort = document.getElementById('active-port');
+const standByIp = document.getElementById('stand-by-ip');
+const standByPort = document.getElementById('stand-by-port');
+
+async function paintHAInfo() {
+    const req = await HA_API.get("/CMMHA");
+    const HA_info = await req.json();
+
+    console.log(HA_info);
+    const { ActiveIP, ActivePort, StandbyIP, StandbyPort} = HA_info;
+
+    activeIp.innerText = ActiveIP ?? "unknown";
+    activePort.innerText = ActivePort ?? "unknown";
+    standByIp.innerText = StandbyIP ?? "unknown";
+    standByPort.innerText = StandbyPort ?? "unknown";
+}
+
+async function init() {
+    paintHAInfo();
 }
 
 init();
